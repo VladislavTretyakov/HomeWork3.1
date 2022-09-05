0f19ff110d8b88a5deb65034d0cdbc0f750d595f
Vladislav Tretyakov <ididnothing86@gmail.com>
2022-09-05 13:12:26 +0600

    initial

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+
+        public int calculate(int price) {
+
+            int onePoint = 20;
+            int cost = price / onePoint;
+            return cost;
+        }
+
+    }
+
